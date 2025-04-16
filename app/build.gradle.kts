12433c9f450c2219af15f3d3590d3103987d5926
Samyak01AI <samyakpawar669@gmail.com>
2025-04-16 16:57:40 +0530

    Simple function for adding Issues and Comments done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -42,7 +43,15 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.auth)
+    implementation(libs.androidx.credentials)
+    implementation(libs.androidx.credentials.play.services.auth)
+    implementation(libs.googleid)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("com.google.firebase:firebase-firestore-ktx:24.10.0")
+    implementation("com.google.firebase:firebase-auth-ktx:22.3.0")
 }
--- app/src/main/java/com/example/civic_trackapplication/MainActivity.kt
+++ app/src/main/java/com/example/civic_trackapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.civic_trackapplication
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        Toast.makeText(this, "Hello World!", Toast.LENGTH_SHORT).show()
+        val o1 = IssuesandComments()
+        o1.createOrCommentOnIssue("issueId", "title", "description", "status", "userComment")
+
     }
 }
